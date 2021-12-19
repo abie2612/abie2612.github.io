ed3d81216041f94d449a9891bd795e5dffc93f7a
Abhishek Bagra <64518678+abie2612@users.noreply.github.com>
2021-12-19 11:31:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["B.Tech Student", "Web Developer", "Graphic Designer"],
+        strings: ["B.Tech Student", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["B.Tech Student", "Web Developer", "Graphic Designer"],
+        strings: ["B.Tech Student", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
